ab61274e31b954c264fe638b0c55ac148df1f6b3
panzerstadt <tliqun@gmail.com>
2019-03-13 21:18:56 +0900

    reducer version

--- src/components/Fetch/index-noreducer.js
+++ src/components/Fetch/index-noreducer.js
@@ -1,42 +1,7 @@
-/*
-A good indicator that they belong together is that
-they are used one after another (e.g. setIsError,
-setIsLoading). Let’s combine all three of them
-with a Reducer Hook instead.
-*/
-
-// useReducer == do similar things together as a group
-
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import styles from "./index.module.css";
-
-// grouped state
-const dataFetchReducer = (state, action) => {
-  switch (action.type) {
-    case "FETCH_INIT":
-      return {
-        ...state,
-        isLoading: true,
-        error: ""
-      };
-    case "FETCH_SUCCESS":
-      return {
-        ...state,
-        isLoading: false,
-        data: action.payload
-      };
-    case "FETCH_FAILURE":
-      return {
-        ...state,
-        isLoading: false,
-        error: action.payload
-      };
-    default:
-      throw new Error("dunno what to do with this condition");
-  }
-};
 
 const useFetch = (initialUrl, initialData) => {
   const INIT = { hits: [] };
@@ -44,48 +9,6 @@ const useFetch = (initialUrl, initialData) => {
   const [url, setUrl] = useState(
     initialUrl || "http://hn.algolia.com/api/v1/search?query=redux"
   );
-
-  const [state, dispatch] = useReducer(dataFetchReducer, {
-    // dataFetchReducer is the action
-    // this here is the initial state
-    isLoading: false,
-    error: "",
-    data: initialData
-  });
-
-  useEffect(() => {
-    let didCancel = false;
-
-    const fetchData = async () => {
-      dispatch({ type: "FETCH_INIT" });
-
-      try {
-        const result = await axios(url);
-        if (!didCancel) {
-          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
-        }
-      } catch (e) {
-        if (!didCancel) {
-          dispatch({ type: "FETCH_FAILURE", payload: e.message });
-        }
-      }
-    };
-
-    fetchData();
-
-    return () => {
-      // if it unmounts, this returns true
-      // thereby skipping FETCH_SUCCESS / FETCH_FAILURE
-      didCancel = true;
-    };
-  }, [url]);
-
-  const doFetch = url => {
-    setUrl(url);
-  };
-
-  return { ...state, doFetch };
-
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
   useEffect(() => {
@@ -105,6 +28,10 @@ const useFetch = (initialUrl, initialData) => {
 
     fetchHN();
   }, [url]);
+
+  const doFetch = url => {
+    setUrl(url);
+  };
 
   return { data, isLoading, error, doFetch };
 };
@@ -148,10 +75,6 @@ const FetchAndDisplay = ({ search }) => {
         </button>
       </form>
 
-      <div className={styles.loadingOrError}>
-        <LoadingOrEror />
-      </div>
-
       <ul>
         {data.hits.map(item => (
           <li key={item.objectID}>
@@ -161,6 +84,9 @@ const FetchAndDisplay = ({ search }) => {
           </li>
         ))}
       </ul>
+      <div className={styles.loadingOrError}>
+        <LoadingOrEror />
+      </div>
     </>
   );
 };
